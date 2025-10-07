72362865d6a56274e0dc084cd685b6dccd5ec852
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-10-07 14:09:03 +0200

    added gateway, discovery and config-server modules

---
+++ config-server/src/test/java/com/bash/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
