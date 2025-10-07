2115031bdfe33c70584a0ca39855b09430ca84d4
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-10-07 17:52:05 +0200

    service annotated and injected StudentClientService properly and enabled feignclient in startup class. able to call student now

--- school/src/main/java/com/bash/school/services/SchoolService.java
+++ school/src/main/java/com/bash/school/services/SchoolService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class SchoolService {
 
     private final SchoolRepository schoolRepository;
-    private StudentClientService studentClientService;
+    private final StudentClientService studentClientService;
 
     public void saveSchool(School school){
         schoolRepository.save(school);
--- school/src/main/java/com/bash/school/services/client/StudentClientService.java
+++ school/src/main/java/com/bash/school/services/client/StudentClientService.java
@@ -2,6 +2,7 @@ package com.bash.school.services.client;
 
 import com.bash.school.models.Student;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @FeignClient(name = "student-service",
     url = "${application.config.students-url}"
 )
+@Service
 public interface StudentClientService {
     @GetMapping("/school/{school-id}")
     List<Student> findAllStudentsBySchool(
